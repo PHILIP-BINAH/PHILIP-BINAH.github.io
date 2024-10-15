cfae03b58b32ad6552c367fea6c27e28aaa18698
Philip-Binah <binahphilip@gmail.com>
2024-10-15 17:56:49 +0000

    first commit

---
+++ data/profile.js
@@ -0,0 +1,130 @@
+const profileData = {
+  title: "Resume",
+  name: "Philip Binah",
+  sub_title: "DevOps Engineer",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `I am a versatile, tech-savvy, and proactive DevOps Engineer with 3+ years of experience in IT systems engineering, focusing on infrastructure management such as server deployment, network configuration, and system monitoring. I am detail-oriented, passionate about learning and, thrive in fast-paced, dynamic teams where I can contribute and align my skills with the company's goals to ensure operational success and drive innovative solutions.`,
+    contact: {
+      email: "binahphilip@gmail.com",
+      phone: "+233 546511134",
+      address: "Ghana",
+    },
+  },
+  links: [
+    {
+      title: "LinkedIn",
+      src: "www.linkedin.com/in/philipbinah",
+      iconClass: "fa-brands fa-linkedin-in",
+    },
+    {
+      title: "Github",
+      src: "https://github.com/PHILIP-BINAH",
+      iconClass: "fa-brands fa-github",
+    },
+  ],
+  skills: [
+    {
+      title: "Operating System",
+      value: "Linux, Windows, iOS",
+    },
+    {
+      title: "Languages",
+      value: "Bash, JavaScript, C++ & Python Scripting",
+    },
+    {
+      title: "Version Control",
+      value: "Git, GitHub",
+    },
+    {
+      title: "CI-CD",
+      value: "Jenkins, GitHub Actions",
+    },
+    {
+      title: "Container & Orchestration",
+      value: "Docker, Kubernetes",
+    },
+    {
+      title: "Monitoring & Observability",
+      value: "Prometheus, Grafana, Nagios and, Zabbix",
+    },
+    {
+      title: "Cloud Computing",
+      value: "AWS and Azure",
+    },
+    {
+      title: "Infrastructure-as-Code & Configuration Mgt.",
+      value: "Terraform, Ansible",
+    },
+  ],
+  experiences: [
+    {
+      organization: "Jomacs",
+      title: "<strong>Systems Administrator</strong> ",
+      date: "December 2021 to Current",
+      details: [
+        `Installed, configured, and <strong>maintained Linux servers</strong> for high availability and reliability.`,
+        `Administered user accounts, groups, and file system permissions.`,
+        `Monitored system performance with <strong>Nagios, Zabbix, CheckMK, and Grafana,</strong> optimizing for high availability, security, and performance.`,
+        `Implemented security measures, managed <strong>firewalls,</strong> and <strong>applied updates/patches</strong> to safeguard servers.`,
+        `Developed and <strong>managed backups and disaster recovery plans</strong> using <strong>cron jobs.</strong>`,
+        `Wrote and maintained <strong>shell script</strong> for <strong>task automation.</strong>`,
+        `Configured network interfaces, <strong>DNS, DHCP, and VPN services.</strong>`,
+        `Diagnosed and resolved connectivity issues; provided user support.`,
+        `Managed <strong>system logs,</strong> analyzed issues, and performed <strong>root cause analysis.</strong>`,
+        `Administered <strong>VMware virtual environments</strong> and managed <strong>AWS and Azure cloud infrastructure.</strong>`,
+
+  ],
+},
+  ],
+  
+  projects: [
+    {
+      title: "Web Hosting and Load Balancing:",
+      duration: "2024",
+      link: "",
+      desc: `Built and configured personal Linus servers to host a simple website using Nginx and Apache ensuring and achieving 99.9% uptime and 25% faster page loads.`,
+    },
+    {
+      title: "Linux Server Administration:",
+      duration: "2024",
+      link: "",
+      desc: `Setup and configured Linux servers (instances) on AWS to host and test personal projects, and undertake several administration tasks using the CLI and basic bash scripts`,
+    },
+    {
+      title: "Set up Monitoring systems/Servers:",
+      duration: "2024",
+      link: "",
+      desc: `Configured and set up monitoring tools such as Nagios, Zabbix, and CheckMK on a Linux server to monitor my servers and services (Grafana and Prometheus are next)`,
+    }
+  ],
+  education: [
+    {
+      alma: "University of Ghana",
+      duration: "Oct 2010 to Julu 2013",
+      std: "Bachelor of Science in Business Administration (Banking & Finance)",
+      score: "81.25%",
+    },
+    {
+      alma: "Ho Technical University, GHANA",
+      duration: "Aug 1999 to May 2002",
+      std: "HND Agricultural Engineering",
+      score: "79.97%",
+    },
+  ],
+  certifications: [
+    {
+      desc: `<strong>AWS Knowledge: Cloud Essentials</strong>.`,
+      date: "July 2024",
+    },
+    {
+      desc: `<strong>AWS Cloud Practitioner</strong>.`,
+      date: "August 2024",
+    },
+    {
+      desc: `<strong>NGD Linux unhatched</strong>.`,
+      date: "September 2024",
+    },
+  ],
+  events: [],
+};
